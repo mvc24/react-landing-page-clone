285d9144e6b280bf25ff78ff4a7ea395c8152d53
Maria <mvalenciacuberos@gmail.com>
2023-09-18 15:31:35 +0200

    Create basic HTML structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import './App.css';
-import logo from './logo.svg';
+import React from 'react';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <nav>
+        <ul>
+          <li>Parents</li>
+          <li>Sitters</li>
+          <li>Trust & Safety</li>
+          <li>Blog</li>
+        </ul>
+        <div>
+          <div>Log in</div>
+          <div>Sign up</div>
+        </div>
+      </nav>
+      <header>
+        <div>Hero section</div>
+        <div>card</div>
       </header>
-    </div>
+      <main>
+        <section>how it works</section>
+        <section>
+          testimonials
+          <div>
+            slider
+            <div>9 slider cards</div>
+          </div>
+        </section>
+        <section>
+          Featured in
+          <div>card</div>
+        </section>
+        <section>
+          <div>why otter</div>
+          <div>
+            two cards
+            <div>card 1</div>
+            <div>card 2</div>
+          </div>
+          <div>card</div>
+        </section>
+        <section>
+          <div>
+            <h3>Common questions</h3> <a>view all FAQs</a>
+            <ul>
+              {' '}
+              expanding cards
+              <li>What is Otter?</li>
+              <li>How is Otter different from other childcare options?</li>
+              <li>Does Otter offer last-minute backup care?</li>
+              <li>How are Otter's sitters vetted?</li>
+              <li>Where is Otter available?</li>
+            </ul>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <div>footer card dark</div>
+        <div>footer card light</div>
+      </footer>
+    </>
   );
 }
